c346875fcbf946d927734e88edcb1f35333171ff
Vidyasagar Bhargava <vidyasagarbhargava@users.noreply.github.com>
2023-03-21 10:26:27 +0530

    mean_square added
    some minor changes

--- data_preparation.py
+++ data_preparation.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pandas as pd
 from sklearn import linear_model
+from sklearn import mean_squared_error
+
+df = pd.read_csv('some_file.csv')
+
+X_train, y_train , X_test, y_test = some_func(df)
 
 regr = linear_model.LinearRegression()
 regr.fit(X_train,y_train)
-predictions = regr.predict(X_test)
+test_predict = regr.predict(X_test)
+
+print(mean_squared_error(y_test, test_predict)
 
 print(regr.coef_)
-
-
-
